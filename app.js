71abfa4bd314a4951c3e61a1171a161ecbd4e822
Sayantan Dey <92203629+SayanTan772@users.noreply.github.com>
2023-07-09 12:43:01 +0530

    Created app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const Observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+  
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => Observer.observe(el));
